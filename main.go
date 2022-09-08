aecd9d85a9d561cb7cafa3d20ee59ad99c51d0f0
f5f5f5f5f5 <ttstll365@gmail.com>
2022-09-08 22:08:15 +0300

    2nd

--- main.go
+++ main.go
@@ -4,23 +4,18 @@ import (
 	"knocker/1009/handler"
 )
 
-type Note struct {
-	id     int
-	name   string
-	text   string
-	access []int
-	ttl    int
-}
+// type Note struct {
+// 	id     int
+// 	name   string
+// 	text   string
+// 	access []int
+// 	ttl    int
+// }
 
 func main() {
-	Notes := make(map[string]Note)
+	//Notes := make(map[string]Note)
 	//Users := make(map[string]string)
-	var a Note
-	a.name = "Nazvanie zametOCHKi"
-	a.text = "Ya ebal amerov"
-	a.access = append(a.access, 212)
-	a.ttl = 112
-	a.id = 212
-	Notes["12131"] = a
+	//var a Note
+	//Notes["12131"] = a
 	handler.HandleRequest()
 }
